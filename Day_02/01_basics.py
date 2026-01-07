1fe596cc9629aab7262c77aa1ca0d0fb08ad9285
Jiyansh Gokhru <jiyanshjain50@gmail.com>
2026-01-07 14:02:32 +0530

    Add basic user input and type display functionality

---
+++ Day_02/01_basics.py
@@ -0,0 +1,11 @@
+
+name = input("Enter your Name :")
+age = int(input("Enter your Age :"))
+branch = input("Enter your Branch :")
+
+print(f"\nName: {name}")
+print(f"Age: {age}")
+print(f"Branch: {branch}\n")
+print(type(name))
+print(type(age))
+print(type(branch))
