23c371a225607aa2a0efd76314a845aaaa375723
Jiyansh Gokhru <jiyanshjain50@gmail.com>
2026-01-09 18:47:11 +0530

    Add greeting and addition functions in functions.py

---
+++ Day_05/functions.py
@@ -0,0 +1,19 @@
+# Function
+def greet():
+    print("Welcome to Data Science!")
+
+greet()
+
+# Function with parameters
+def greet_name(name):
+    print("Hello", name)
+
+greet_name("Jiyansh")
+
+# Function with return
+def add(a, b):
+    return a + b
+
+result = add(10, 5)
+print(result)
+
